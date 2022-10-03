c29916758e4517a7d3dd0dee62249823665c29b3
DrPRock <113452492+DrPRock@users.noreply.github.com>
2022-10-03 13:12:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,3 +7,21 @@ btnE1.addEventListener("mouseover", (event) => {
   btnE1.style.setProperty("--xPos", x + "px");
   btnE1.style.setProperty("--yPos", y + "px");
 });
+
+
+      const pictures = ['batEarrings.jpg', 'batNecklace.jpg', 'candyWitchEarrings.jpg'];
+      const picture = pictures[Math.floor(Math.random() * pictures.length)];
+      document.getElementById("thumb").innerHTML = ('<img class=right1 src="' + picture + '"/>');
+
+      const pictures1 = ['bax1.jpg', 'mandala.jpg', 'necklaceStar.jpg'];
+      const picture1 = pictures1[Math.floor(Math.random() * pictures1.length)];
+      document.getElementById("thumb1").innerHTML = ('<img class=right1 src="' + picture1 +  '"/>');
+
+      const pictures2 = ['palmTree.jpg', 'spoonRestA.jpg', 'necklaceSilverRect.jpg'];
+      const picture2 = pictures2[Math.floor(Math.random() * pictures2.length)];
+      document.getElementById("thumb2").innerHTML = ('<img class=right1 src="' + picture2 + '"/>');
+
+      const pictures3 = ['largtrayButterfly.jpg', 'sloth.jpg', 'turtle.jpg'];
+      const picture3 = pictures3[Math.floor(Math.random() * pictures3.length)];
+      document.getElementById("thumb3").innerHTML = ('<img class=right1 src="' + picture3 + '"/>');
+ 
