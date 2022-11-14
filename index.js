040eb9035a4e8ed1ac7ac283a76e3572384021e4
DrPRock <113452492+DrPRock@users.noreply.github.com>
2022-11-13 23:40:57 -0600

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ btnE1.addEventListener("mouseover", (event) => {
   const shoulder = document.querySelector('.right')
 
       function getRandNum(){
-        return Math.floor(Math.random() * 147)
+        return Math.floor(Math.random() * 276)
       }
       
       function sideImages(numImages = 7){
@@ -31,7 +31,7 @@ btnE1.addEventListener("mouseover", (event) => {
   const container = document.querySelector('.scrollContainer')
 
       function getRandNum(){
-        return Math.floor(Math.random() * 147)
+        return Math.floor(Math.random() * 276)
       }
       
       function loadImages(numImages = 12){
