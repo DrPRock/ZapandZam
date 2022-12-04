4200c141509b429ff231ba574d98a71a1762d348
DrPRock <113452492+DrPRock@users.noreply.github.com>
2022-12-04 01:59:11 -0600

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@
           i++
         }
       }
-      
+      loadImages()
  //listen for scroll
       
       window.addEventListener('scroll', () => {
@@ -23,3 +23,28 @@
           loadImages()
         }
       })
+//button color
+const containerE1 = document.querySelector(".btnL");
+
+const colorContainerE1s = document.querySelectorAll("button");
+
+generateColors();
+
+function generateColors() {
+  colorContainerE1s.forEach(
+    (colorContainerE1) => {
+    const newColorCode = randomColor();
+    colorContainerE1.style.backgroundColor = "#" + newColorCode;
+  });
+}
+
+function randomColor() {
+  const chars = "0123456789abcdef";
+  const colorCodeLength = 6;
+  let colorCode = "";
+  for (let index = 0; index < colorCodeLength; index++) {
+    const randomNum = Math.floor(Math.random() * chars.length);
+    colorCode += chars.substring(randomNum, randomNum + 1);
+  }
+  return colorCode;
+}
