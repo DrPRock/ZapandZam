a1d2ca4b7253a01e698990c09b1bae7065821c92
DrPRock <113452492+DrPRock@users.noreply.github.com>
2022-12-22 18:44:19 -0600

    new

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
   const container = document.querySelector('.scrollContainer')
 
       function getRandNum(){
-        return Math.floor(Math.random() * 463)
+        return Math.floor(Math.random() * 504)
       }
       
       function loadImages(numImages = 20){
