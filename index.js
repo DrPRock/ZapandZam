b6fd61e9b31082092322fb4d1f62cb158e2cdcc2
DrPRock <113452492+DrPRock@users.noreply.github.com>
2023-03-08 16:45:51 -0600

    Update index.js

--- index.js
+++ index.js
@@ -29,29 +29,3 @@
     }
   }, 2);
 }, { passive: true });
-
-//button color
-const containerE1 = document.querySelector(".btnL");
-
-const colorContainerE1s = document.querySelectorAll("button");
-
-generateColors();
-
-function generateColors() {
-  colorContainerE1s.forEach(
-    (colorContainerE1) => {
-    const newColorCode = randomColor();
-    colorContainerE1.style.backgroundColor = "#" + newColorCode;
-  });
-}
-
-function randomColor() {
-  const chars = "0123456789abcdef";
-  const colorCodeLength = 6;
-  let colorCode = "";
-  for (let index = 0; index < colorCodeLength; index++) {
-    const randomNum = Math.floor(Math.random() * chars.length);
-    colorCode += chars.substring(randomNum, randomNum + 1);
-  }
-  return colorCode;
-}
