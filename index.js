88780fc5a4e0fcb95b81b594addf47e8e5de4ba9
DrPRock <113452492+DrPRock@users.noreply.github.com>
2023-11-10 19:11:33 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+// Get all taglines and hide them
+
+    const taglines = document.querySelectorAll('.tagline');
+    taglines.forEach(tagline => {
+      tagline.style.display = 'none';
+});
+
+// Pick a random tagline and show it
+
+    const randomIndex = Math.floor(Math.random() * taglines.length);
+    taglines[randomIndex].style.display = 'block';
+
 //new featured link
 const imgElements = document.querySelectorAll('.newChild img');
 
@@ -37,18 +49,6 @@ imgElements.forEach(img => {
     }
   }, 2);
 }, { passive: true });
-
-// Get all taglines and hide them
-
-    const taglines = document.querySelectorAll('.tagline');
-    taglines.forEach(tagline => {
-      tagline.style.display = 'none';
-});
-
-// Pick a random tagline and show it
-
-    const randomIndex = Math.floor(Math.random() * taglines.length);
-    taglines[randomIndex].style.display = 'block';
 
 // Parallax effect
 window.addEventListener("scroll", function() {
