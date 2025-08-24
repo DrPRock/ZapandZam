de3fe1588ed4f9795c8c833e63657232d482e53d
DrPRock <113452492+DrPRock@users.noreply.github.com>
2025-08-24 11:44:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,16 +10,6 @@
     const randomIndex = Math.floor(Math.random() * taglines.length);
     taglines[randomIndex].style.display = 'block';
 
-//new featured link
-const imgElements = document.querySelectorAll('.newChild img');
-
-imgElements.forEach(img => {
-  img.addEventListener('click', function() {
-    const url = this.getAttribute('data-url');
-    window.open(url, '_blank');
-  });
-});
-
 // Parallax effect
 window.addEventListener("scroll", function() {
   var y = window.scrollY;
@@ -42,3 +32,4 @@ window.addEventListener("scroll", function() {
         scroller.appendChild(duplicatedItem);
     });
 });
+
